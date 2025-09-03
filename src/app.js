a99734c5a84419f0d68455fe38d08074cf9ca0ba
erivera1993 <erivera1942@gmail.com>
2025-09-03 02:01:31 +0000

    completed work

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
+const excuses = [
+  "My dog ate my homework.",
+  "I thought it was due next week.",
+  "Aliens abducted me and just returned me.",
+  "My computer exploded.",
+  "I was busy binge-watching a series.",
+  "There was a traffic jam in my hallway.",
+  "My goldfish needed emotional support.",
+  "I joined a secret mission. Can't talk about it.",
+  "My alarm didn't go off... again.",
+  "I slipped into a parallel universe."
+];
 
+const excuseElement = document.getElementById("excuse");
+const button = document.getElementById("generateBtn");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+button.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * excuses.length);
+  excuseElement.textContent = excuses[randomIndex];
+});
